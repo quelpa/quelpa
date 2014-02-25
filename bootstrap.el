2040799900095bb79a1d9f60f159b9ca9d4ee59e
steckerhalter <steckerhalter@postmomentum.ch>
2014-02-25 19:27:55 +0100

    use temp dir, fixes #24

--- bootstrap.el
+++ bootstrap.el
@@ -14,34 +14,38 @@
 (require 'package)
 
 (defvar quelpa-ci-dir nil
-  "If non-nil, another directory will be used by Travis.")
+  "If non-nil, quelpa will not be loaded from the url but from the given dir.")
 
 ;; `package' has to be initialized to install pkgs
 (package-initialize)
 
-;; if we do not have melpa's package-build, get the tar package and install it
-(unless (require 'package-build nil t)
-  (let* ((archive "http://melpa.milkbox.net/packages/")
-         (archive-contents (with-temp-buffer
-                             (url-insert-file-contents (concat archive "archive-contents"))
-                             (cdr (read (current-buffer)))))
-         (archive-entry (assoc 'package-build archive-contents))
-         (archive-file-name (let* ((name (car archive-entry))
-                                   (pkg-info (cdr archive-entry))
-                                   (version (package-version-join (aref pkg-info 0)))
-                                   (flavour (aref pkg-info 3)))
-                              (format "%s-%s.%s" name version (if (eq flavour 'single) "el" "tar"))))
-         (archive-url (concat archive archive-file-name))
-         (file (expand-file-name (concat temporary-file-directory archive-file-name))))
-    (url-copy-file archive-url file t)
-    (package-install-file file)))
+(let ((temp-dir (make-temp-file "quelpa" t)))
 
-;; since we have package-build now, we can install the quelpa package
-(unless (require 'quelpa nil t)
-  (let ((file (or (when quelpa-ci-dir (concat quelpa-ci-dir "/quelpa.el"))
-                  (expand-file-name (concat temporary-file-directory "quelpa.el")))))
-    (unless quelpa-ci-dir
-      (url-copy-file "https://raw.github.com/quelpa/quelpa/master/quelpa.el" file t))
-    (package-install-file file)))
+  ;; if we do not have melpa's package-build, get the tar package and install it
+  (unless (require 'package-build nil t)
+    (let* ((archive "http://melpa.milkbox.net/packages/")
+           (archive-contents (with-temp-buffer
+                               (url-insert-file-contents (concat archive "archive-contents"))
+                               (cdr (read (current-buffer)))))
+           (archive-entry (assoc 'package-build archive-contents))
+           (archive-file-name (let* ((name (car archive-entry))
+                                     (pkg-info (cdr archive-entry))
+                                     (version (package-version-join (aref pkg-info 0)))
+                                     (flavour (aref pkg-info 3)))
+                                (format "%s-%s.%s" name version (if (eq flavour 'single) "el" "tar"))))
+           (archive-url (concat archive archive-file-name))
+           (file (expand-file-name archive-file-name temp-dir)))
+      (url-copy-file archive-url file t)
+      (package-install-file file)))
+
+  ;; since we have package-build now, we can install the quelpa package
+  (unless (require 'quelpa nil t)
+    (let ((file (or (when quelpa-ci-dir (concat quelpa-ci-dir "/quelpa.el"))
+                    (expand-file-name "quelpa.el" temp-dir))))
+      (unless quelpa-ci-dir
+        (url-copy-file "https://raw.github.com/quelpa/quelpa/master/quelpa.el" file t))
+      (package-install-file file)))
+
+  (delete-directory temp-dir t))
 
 ;;; bootstrap.el ends here
