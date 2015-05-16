e623cc26f1f5ed7141b775b8d8ac8776a50de687
steckerhalter <steckerhalter@postmomentum.ch>
2015-05-16 13:29:19 +0200

    fix link to readme

--- quelpa.el
+++ quelpa.el
@@ -31,8 +31,8 @@
 ;; Your personal local Emacs Lisp Package Archive (ELPA) with packages
 ;; built on-the-fly directly from source.
 
-;; See the README.org for more info:
-;; https://github.com/quelpa/quelpa/README.org
+;; See the README for more info:
+;; https://github.com/quelpa/quelpa/blob/master/README.md
 
 ;;; Requirements:
 
