32e8a91792e4e9fce7bfb860b16a8c805889e12f
steckerhalter <steckerhalter@postmomentum.ch>
2017-12-12 19:09:15 +0100

    build emacs from source

--- ci/run.sh
+++ ci/run.sh
@@ -12,10 +12,28 @@ else
     dir="$PWD"
 fi
 
+pwd=`pwd`
+
+if ! test -e /usr/local/bin/emacs; then
+    {
+        >&2 echo "--- installing deps ---"
+        sudo apt-get update
+        sudo apt-get -y install build-essential wget git mercurial
+        sudo apt-get -y build-dep emacs23
+        wget http://ftp.gnu.org/gnu/emacs/emacs-25.3.tar.gz -O- | tar xz
+        cd emacs-25.3
+        >&2 echo "--- building emacs ---"
+        ./configure &&\
+            make &&\
+            sudo make install
+        sudo apt-get install -y -qq git
+    } > /dev/null
+fi
+
+echo "--- running tests ---"
+
 # ERT tests
-emacs24 --batch --eval "(setq quelpa-ci-dir \"$dir\")" --load ert --load $dir/test/quelpa-test.el --funcall ert-run-tests-batch-and-exit
-rm -rf ~/.emacs.d/
-emacs-snapshot --batch --eval "(setq quelpa-ci-dir \"$dir\")" --load ert --load $dir/test/quelpa-test.el --funcall ert-run-tests-batch-and-exit
+emacs --batch --eval "(setq quelpa-ci-dir \"$dir\")" --load ert --load $dir/test/quelpa-test.el --funcall ert-run-tests-batch-and-exit
 rm -rf ~/.emacs.d/
 
 # For the file fetcher in `ci/.emacs`.
@@ -23,8 +41,6 @@ git clone https://github.com/emacs-helm/helm ~/emacs-packages/helm
 git clone https://github.com/jwiegley/use-package ~/emacs-packages/use-package
 
 # functional tests
-emacs24 --batch --eval "(setq quelpa-ci-dir \"$dir\")" --load $dir/ci/.emacs
-rm -rf ~/.emacs.d/
-emacs-snapshot --batch --eval "(setq quelpa-ci-dir \"$dir\")" --load $dir/ci/.emacs
+emacs --batch --eval "(setq quelpa-ci-dir \"$dir\")" --load $dir/ci/.emacs
 rm -rf ~/.emacs.d/
 rm -rf ~/emacs-packages/
